adf1b30f7237a74f784df1cea0a9138db0a2746b
Roman <mailto.32roman@gmail.com>
2021-09-05 22:49:58 +0300

    adding cards array

--- index.js
+++ index.js
@@ -3,26 +3,38 @@ let firstCard = 11,
   sumOfCards = firstCard + secondCard,
   hasBlackJack = false,
   isAlive = true,
-message = "",
-messageEl = document.getElementById("message-el"),
-sumEl = document.getElementById("sum-el"),
-cardsEl = document.getElementById("cards-el");
+  message = "",
+  messageEl = document.getElementById("message-el"),
+  sumEl = document.getElementById("sum-el"),
+  cardsEl = document.getElementById("cards-el");
+let myCards = [firstCard, secondCard];
 
 function startGame() {
-if (sumOfCards <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sumOfCards === 21) {
-  message = "Woohoo! You`ve got blackjack!";
-  hasBlackJack = true;
-} else {
-  message = "You`re out of the game!";
-  isAlive = false;
+  renderGame();
 }
-messageEl.textContent = message;
-sumEl.textContent = "Sum: " + sumOfCards;
-cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+
+function renderGame() {
+  if (sumOfCards <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sumOfCards === 21) {
+    message = "Woohoo! You`ve got blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You`re out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sumOfCards;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < myCards.length; i++) {
+    cardsEl.textContent += myCards[i] + " ";
+  }
 }
 
 function newCard() {
-    console.log("Draw a new card")
+  console.log("Draw a new card");
+  let card = 7;
+  sumOfCards += card;
+  myCards.push(card);
+  renderGame();
 }
